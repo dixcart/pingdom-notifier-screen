69ed3b583c2bbd46aa6419f8947175d95504f3ba
Richard Benson <richard.benson@dixcart.com>
2013-08-23 10:23:59 +0100

    Remove leftover guff from Splice status screen

--- index.php
+++ index.php
@@ -44,9 +44,7 @@
 
 			//Run checks and set intervals
 			fastChecks();
-			//setTimeout(slowChecks, 5 * 1000);
 			timer = setInterval(fastChecks, 60 * 1000);
-			//timer2 = setInterval(slowChecks, 5 * 60 *1000);
 	});
 
 	function fastChecks() {
@@ -76,97 +74,6 @@
 
 	}
 
-	function dialogSSH(strCommand, strServerIP, strServerName) {
-        $('<div title="' + strServerName + '"><pre class="server-response"><img src="assets/images/ajax-loader.gif" /></pre></div>').dialog({
-            resizable: true,
-            height: 300,
-            width: 600,
-            modal: false,
-            open: function() {
-            	var strUrl = "";
-            	switch (strCommand)
-            	{
-            		case "time":
-            			strUrl = "sync-times.php";
-            			break;
-            		case "update":
-            			strUrl = "download-patch.php";
-            			break;
-            		case "disk-space":
-            			strUrl = "disk-space.php";
-            			break;
-            	}
-            	$.ajax({
-            		url: 'ajax/' + strUrl,
-            		data: "ip=" + strServerIP,
-            		context: this
-            	}).done(function(data) {
-            		$(this).children('.server-response').html(data);
-            	})
-            },
-            close: function() {
-            	$(this).remove();
-            }
-        });		
-	}
-
-	function confirmDialog(strMessage, onOK) {
-        $('<div title="Confirm"><p>' + strMessage + '</p></div>').dialog({
-            resizable: false,
-            modal: true,
-            buttons: {
-                "OK": function() {
-                	$(this).dialog("close").remove();
-                	onOK();
-                },
-                Cancel: function() {
-                    $(this).dialog("close").remove();
-                }
-            }
-        });		
-	}
-
-	function slowChecks() {
-		times = new Array();
-		versions = new Array();
-		$('.server, .module').each(function() {
-			//Only run if server is available
-			if ($(this).hasClass('up')) {
-				$.ajax({
-					url: 'ajax/get-server-time.php',
-					data: "ip=" + $(this).attr('data-ip') + "&type=" + $(this).attr('data-type'),
-					context: this
-				}).done(function(data) {
-					$(this).children('.server-time').html(data);
-					times.push(data);
-					times.sort();
-					if (times[0] == times[times.length - 1]) $('#time-variance').html(times[0]);
-					else $('#time-variance').html(times[0] + ' - ' + times[times.length - 1]);
-				})
-				$.ajax({
-					url: 'ajax/get-server-version.php',
-					data: "ip=" + $(this).attr('data-ip') + "&type=" + $(this).attr('data-type'),
-					context: this
-				}).done(function(data) {
-					$(this).children('.server-version').html(data);
-					if (versions.indexOf(data) == -1) versions.push(data);
-					versions.sort();
-					$('#versions').html(versions.join(', '));
-				})
-				if ($(this).hasClass('server')) {
-					$.ajax({
-						url: 'ajax/mount-usage.php',
-						data: "ip=" + $(this).attr('data-ip'),
-						context: this
-					}).done(function(data) {
-						$(this).children('.disk-space').children('.progress-inner').progressbar({value: parseInt(data['\/']) });
-						$(this).children('.voicemail-space').children('.progress-inner').progressbar({value: parseInt(data['\/voicemail']) });
-						$(this).children('.log-space').children('.progress-inner').progressbar({value: parseInt(data['\/logs']) });
-					})
-				}
-			}
-		});
-	}
 
 </script>
 
